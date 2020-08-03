132849ae0d62870c9cfa659cd34669f4f1d9ca81
Cédric Belin <cedric@belin.io>
2020-08-04 01:20:51 +0200

    Added the unit tests of the `Blog` class

---
+++ test/akismet/BlogTest.hx
@@ -0,0 +1,20 @@
+package akismet;
+
+/** Tests the features of the `Blog` class. **/
+class BlogTest extends Test {
+
+	/** Tests the `toJson()` method. **/
+	function testToJson() {
+		// It should return only the blog URL with a newly created instance.
+		var data = new Blog("https://docs.belin.io/akismet.hx").toJson();
+		Assert.equals(1, data.keys().length);
+		Assert.equals("https://docs.belin.io/akismet.hx", data.get("blog"));
+
+		// It should return a non-empty map with an initialized instance.
+		data = new Blog("https://docs.belin.io/akismet.hx", {charset: "UTF-8", languages: ["en", "fr"]}).toJson();
+		Assert.equals(3, data.keys().length);
+		Assert.equals("https://docs.belin.io/akismet.hx", data.get("blog"));
+		Assert.equals("UTF-8", data.get("blog_charset"));
+		Assert.equals("en,fr", data.get("blog_lang"));
+	}
+}
