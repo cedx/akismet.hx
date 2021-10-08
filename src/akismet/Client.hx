decaae2d370f1eccfb9fdbe29cade56cd95cb750
Cédric Belin <cedric@belin.io>
2021-10-08 12:09:35 +0200

    The `Content-Length` header is required

--- src/akismet/Client.hx
+++ src/akismet/Client.hx
@@ -1,5 +1,6 @@
 package akismet;
 
+import haxe.io.Bytes;
 import tink.Url;
 import tink.http.Client as HttpClient;
 import tink.http.Fetch.FetchOptions;
@@ -67,17 +68,19 @@ class Client {
 
 	/** Queries the service by posting the specified fields and returns the response. **/
 	function fetch(url: Url, fields: Map<String, String>) {
-		final headers = [
-			new HeaderField(CONTENT_TYPE, "application/x-www-form-urlencoded"),
-			new HeaderField("user-agent", userAgent)
-		];
-
 		final body = Query.build();
 		for (key => value in blog.toMap()) body.add(key, value);
 		for (key => value in fields) body.add(key, value);
 		if (isTest) body.add("is_test", "1");
 
-		final options: FetchOptions = {method: POST, headers: headers, body: body.toString()};
+		final bytes = Bytes.ofString(body.toString());
+		final headers = [
+			new HeaderField(CONTENT_LENGTH, bytes.length),
+			new HeaderField(CONTENT_TYPE, "application/x-www-form-urlencoded"),
+			new HeaderField("user-agent", userAgent)
+		];
+
+		final options: FetchOptions = {method: POST, headers: headers, body: bytes};
 		return HttpClient.fetch(url, options).all().next(response -> {
 			final akismetHeader = response.header.get("x-akismet-debug-help");
 			akismetHeader.length > 0 ? new Error(UnprocessableEntity, akismetHeader[0]) : response;
