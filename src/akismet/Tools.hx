85249686079285a1c9360bb5461c84c5990278ce
Cédric Belin <cedric@belin.io>
2023-02-13 21:02:16 +0100

    Code optimization

--- src/akismet/Tools.hx
+++ src/akismet/Tools.hx
@@ -2,6 +2,6 @@ package akismet;
 
 using DateTools;
 
-/** Converts the specified date to an ISO 8601 string, using the UTC time zone. **/
-@:noDoc function toIsoString(dateTime: Date)
-	return Date.fromTime(dateTime.getTime() + dateTime.getTimezoneOffset().minutes()).format("%FT%TZ");
+/** Converts the specified `date` to an ISO 8601 string, using the UTC time zone. **/
+@:noDoc function toIsoString(date: Date)
+	return date.delta(date.getTimezoneOffset().minutes()).format("%FT%TZ");
