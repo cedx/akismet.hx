3341a67cf466bbefa00ad0646d80917901f16d99
Cédric Belin <cedric@belin.io>
2023-05-25 23:28:39 +0200

    Update the "Doc" task

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -13,7 +13,7 @@ function main() {
 		"--define", "source-path", "https://github.com/cedx/akismet.hx/blob/main/src",
 		"--define", "themeColor", "0xea8220",
 		"--define", "version", Version.packageVersion,
-		"--define", "website", "https://github.com/cedx/akismet.hx",
+		"--define", "website", "https://docs.belin.io/akismet.hx",
 		"--input-path", "var",
 		"--output-path", "docs",
 		"--title", "Akismet for Haxe",
