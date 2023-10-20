7709ac1da76e555795555e298d44fafcbd60d5ed
Cédric Belin <cedric@belin.io>
2023-10-21 01:05:36 +0200

    Update the `User-Agent` string

--- test/akismet/ClientTest.hx
+++ test/akismet/ClientTest.hx
@@ -17,7 +17,7 @@ package akismet;
 			name: "Akismet",
 			role: Administrator,
 			url: "https://belin.io",
-			userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
+			userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 		}),
 		content: "I'm testing out the Service API.",
 		referrer: "https://lib.haxe.org/p/akismet",
