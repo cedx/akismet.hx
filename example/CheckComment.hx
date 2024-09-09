66094e7234f7a76cae2939876cf63fb87fced249
Cédric Belin <cedric@belin.io>
2024-09-09 12:39:12 +0200

    Update the "User-Agent" string

--- example/CheckComment.hx
+++ example/CheckComment.hx
@@ -11,7 +11,7 @@ function main() {
 		ipAddress: "192.168.123.456",
 		name: "John Doe",
 		role: "guest",
-		userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+		userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0"
 	});
 
 	final comment = new Comment({
